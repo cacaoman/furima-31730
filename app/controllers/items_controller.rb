2a9899b1ac177fb6c46474b1aaed60a669b4e1b7
cacaoman <cacaoman.highend@gmail.com>
2020-12-01 12:46:49 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -22,9 +22,19 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user_id
   end
 
-  
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   private
 
